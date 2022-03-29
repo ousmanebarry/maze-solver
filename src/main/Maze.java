1e3588ea9f3f2e6d39245e5fab835da18438393e
Ousmane Barry <ppobarry@gmail.com>
2022-03-29 11:24:53 -0400

    Get entrance and exit index

--- src/main/Maze.java
+++ src/main/Maze.java
@@ -8,33 +8,55 @@ import java.io.FileNotFoundException;
 
 public class Maze {
 
+  private static final String ENTRANCE_STRING = "E";
+  private static final String EXIT_STRING = "X";
+  private int entranceRowIndex = -1;
+  private int exitRowIndex = -1;
+  private int entranceColIndex;
+  private int exitColIndex;
   private List<List<String>> maze;
   private List<List<String>> solvedMaze;
 
-  public Maze(String fileName) throws FileNotFoundException {
+  public Maze(String fileName) throws FileNotFoundException, IllegalArgumentException {
     File file = new File("mazes/" + fileName + ".txt");
     Scanner scanner = new Scanner(file);
     maze = solvedMaze = new ArrayList<>();
     initialize(scanner);
+    entranceColIndex = 0;
+    exitColIndex = maze.size() - 1;
+
+    if (entranceRowIndex == -1 || exitRowIndex == -1) {
+      throw new IllegalArgumentException("The maze must have an entrance and an exit");
+    }
   }
 
   public void initialize(Scanner scanner) {
 
     while (scanner.hasNext()) {
+      int counter = 0;
       String currentLine = scanner.nextLine();
       String[] arrayCurrentLine = currentLine.split("");
       List<String> listCurrentLine = new ArrayList<>();
 
-      for (String i : arrayCurrentLine)
+      for (String i : arrayCurrentLine) {
         listCurrentLine.add(i);
+        if (i.equals(ENTRANCE_STRING)) {
+          entranceRowIndex = counter;
+        } else if (i.equals(EXIT_STRING)) {
+          exitRowIndex = counter;
+        }
+        counter++;
+      }
 
       maze.add(listCurrentLine);
       solvedMaze.add(listCurrentLine);
     }
 
-    // System.out.println(maze);
-
     scanner.close();
+  }
+
+  public boolean canMove(char direction) {
+    return true;
   }
 
   public int solve() {
@@ -43,14 +65,18 @@ public class Maze {
 
     start = System.currentTimeMillis();
 
-    System.out.println();
+    // System.out.println("Entrance : 0," + entranceRowIndex);
+    // System.out.println(maze.get(0).get(entranceRowIndex));
+    // System.out.println("Exit : " + (maze.size() - 1) + "," + exitRowIndex);
+    // System.out.println(maze.get(maze.size() - 1).get(exitRowIndex));
+    System.out.println(solvedMaze);
 
     end = System.currentTimeMillis();
 
     return (int) (end - start);
   }
 
-  public void getSolvedPath() {
-    System.out.println(solvedMaze);
+  public List<List<String>> getSolvedPath() {
+    return solvedMaze;
   }
 }
