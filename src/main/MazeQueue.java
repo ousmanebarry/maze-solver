b04245ef029ddbd45c308f0ae72c7bbd47445d1e
Ousmane Barry <ppobarry@gmail.com>
2022-03-30 17:43:20 -0400

    Visited places

--- src/main/MazeQueue.java
+++ src/main/MazeQueue.java
@@ -1,9 +1,13 @@
 package main;
 
+import java.util.Queue;
+import java.util.LinkedList;
 import java.io.FileNotFoundException;
 import java.lang.IllegalStateException;
 
 public class MazeQueue extends Maze {
+
+  private boolean visited[][];
 
   public MazeQueue(String fileName) throws FileNotFoundException, IllegalStateException {
     super(fileName);
@@ -11,8 +15,23 @@ public class MazeQueue extends Maze {
 
   public int solve() {
     long start, end;
+    visited = new boolean[maze.size()][maze.get(0).size()];
 
     start = System.currentTimeMillis();
+
+    int counter = 0;
+    Queue<String> q = new LinkedList<String>();
+    q.add("");
+    // q.peek() != "URDRRR"
+    while (counter < 4) {
+      String s = q.remove();
+      q.add(s + "L");
+      q.add(s + "R");
+      q.add(s + "U");
+      q.add(s + "D");
+      System.out.println(q);
+      counter++;
+    }
 
     // System.out.println("Entrance : 0," + entranceColIndex);
     // System.out.println(maze.get(entranceRowIndex).get(entranceColIndex));
